0821988b56c155d08b9b6d29c709abb30e888273
csearafat01 <54732088+csearafat01@users.noreply.github.com>
2023-09-16 22:43:12 +0600

    module_6_assignment

--- lib/main.dart
+++ lib/main.dart
@@ -1,150 +1,136 @@
 import 'package:flutter/material.dart';
 
-/// Navigation - one page -> another page
-/// Navigator 1(Simple and Lengthy), 2 (complex)
-/// Routers Package -> Getx, GoRouter, Auto Route
-
-/// TODO : Stack, Queue, Navigator, Navigation
-
 void main() {
-  runApp(MyApp());
+  runApp(PhotoGalleryApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+class PhotoGalleryApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomeScreen(),
+      title: 'Photo Gallery',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: PhotoGalleryHomePage(),
     );
   }
 }
 
-/// Route
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
-
+class PhotoGalleryHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home'),
+        title: Center(child: Text('Photo Gallery')),
       ),
-      body: Center(
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(8),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              'Home',
-              style: TextStyle(fontSize: 24),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                'Welcome to My Photo Gallery!',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
             ),
-            ElevatedButton(
-              onPressed: () {
-                /// Navigation - Route home -> Route settings
-                /// Navigator
-                /// Navigator.typeOfNavigation(currentLocation, Destination);
-                /// Navigation - Push, Pop(Back), replace, replaceAll, removeUntil
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => SettingsScreen(),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Search for photos...',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                ),
+              ),
+            ),
+            GridView.builder(
+              shrinkWrap: true,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+                crossAxisSpacing: 8,
+                mainAxisSpacing: 8,
+              ),
+              itemCount: 6,
+              itemBuilder: (context, index) {
+                return GestureDetector(
+                  onTap: () {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text('Image ${index + 1} Clicked!'),
+                      ),
+                    );
+                  },
+                  child: Card(
+                    elevation: 6,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Image.network(
+                          'https://hips.hearstapps.com/hmg-prod/images/cute-cat-photos-1593441022.jpg?crop=0.670xw:1.00xh;0.167xw,0&resize=640:*',
+                          height: 84,
+                          fit: BoxFit.cover,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Center(child: Text('Photo ${index + 1}')),
+                        ),
+                      ],
+                    ),
                   ),
                 );
               },
-              child: Text('Go to settings'),
             ),
-            ElevatedButton(
-              onPressed: () {
-                /// Navigation - Route home -> Route settings
-                /// Navigator
-                /// Navigator.typeOfNavigation(currentLocation, Destination);
-                /// Navigation - Push, Pop(Back), replace, replaceAll, removeUntil
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => OrdersScreen(),
-                  ),
-                );
-              },
-              child: Text('Go to Orders'),
+            ListTile(
+              leading: Image.network(
+                'https://hips.hearstapps.com/hmg-prod/images/cute-cat-photos-1593441022.jpg?crop=0.670xw:1.00xh;0.167xw,0&resize=640:*',
+                height: 100,
+                width: 80,
+                fit: BoxFit.cover,
+              ),
+              title: Text('Photo 1'),
+              subtitle: Text('Description for Photo 1'),
             ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-/// Route
-class SettingsScreen extends StatelessWidget {
-  const SettingsScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Settings'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Settings',
-              style: TextStyle(fontSize: 24),
+            ListTile(
+              leading: Image.network(
+                'https://hips.hearstapps.com/hmg-prod/images/cute-cat-photos-1593441022.jpg?crop=0.670xw:1.00xh;0.167xw,0&resize=640:*',
+                height: 100,
+                width: 80,
+                fit: BoxFit.cover,
+              ),
+              title: Text('Photo 2'),
+              subtitle: Text('Description for Photo 2'),
             ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushAndRemoveUntil(context,
-                  MaterialPageRoute(builder: (context) => HomeScreen()),
-                      (route) => false,
-                );
-              },
-              child: Text('Home'),
-            )
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-/// Route
-class OrdersScreen extends StatelessWidget {
-  const OrdersScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Orders'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Orders',
-              style: TextStyle(fontSize: 24),
+            ListTile(
+              leading: Image.network(
+                'https://hips.hearstapps.com/hmg-prod/images/cute-cat-photos-1593441022.jpg?crop=0.670xw:1.00xh;0.167xw,0&resize=640:*',
+                height: 80,
+                width: 80,
+                fit: BoxFit.cover,
+              ),
+              title: Text('Photo 3'),
+              subtitle: Text('Description for Photo 3'),
             ),
-            TextButton(
-              onPressed: () {
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => SettingsScreen(),
-                  ),
-                );
-              },
-              child: Text('Go to settings'),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: FloatingActionButton(
+                elevation: 18,
+                child: Icon(Icons.upload),
+                onPressed: () {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text('Photos Uploaded Successfully!'),
+                    ),
+                  );
+                },
+                //child: Icon(Icons.upload),
+              ),
             ),
-            TextButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: Text('Back to home'),
-            )
           ],
         ),
       ),

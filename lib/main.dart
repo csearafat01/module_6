75a363469b29d9cfce9631d5e58565f153fbdf47
csearafat01 <54732088+csearafat01@users.noreply.github.com>
2023-09-16 23:17:08 +0600

    module_6_live_test

--- lib/main.dart
+++ lib/main.dart
@@ -1,139 +1,63 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(PhotoGalleryApp());
+  runApp(ShoppingListApp());
 }
 
-class PhotoGalleryApp extends StatelessWidget {
+class ShoppingListApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Photo Gallery',
+      debugShowCheckedModeBanner: false,
+      title: 'My Shopping List',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: PhotoGalleryHomePage(),
+      home: ShoppingListPage(),
     );
   }
 }
 
-class PhotoGalleryHomePage extends StatelessWidget {
+class ShoppingListPage extends StatelessWidget {
+  final List<ShoppingItem> shoppingItems = [
+    ShoppingItem(icon: Icons.add_shopping_cart, name: 'Apples'),
+    ShoppingItem(icon: Icons.add_shopping_cart, name: 'Bananas'),
+    ShoppingItem(icon: Icons.add_shopping_cart, name: 'Milk'),
+    ShoppingItem(icon: Icons.add_shopping_cart, name: 'Bread'),
+    ShoppingItem(icon: Icons.add_shopping_cart, name: 'Eggs'),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Center(child: Text('Photo Gallery')),
+        title: Center(child: Text('My Shopping List')),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.shopping_cart),
+            onPressed: () {
+              // Handle cart icon tap
+            },
+          ),
+        ],
       ),
-      body: SingleChildScrollView(
-        padding: EdgeInsets.all(8),
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Text(
-                'Welcome to My Photo Gallery!',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: TextField(
-                decoration: InputDecoration(
-                  hintText: 'Search for photos...',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8.0),
-                  ),
-                ),
-              ),
-            ),
-            GridView.builder(
-              shrinkWrap: true,
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 3,
-                crossAxisSpacing: 8,
-                mainAxisSpacing: 8,
-              ),
-              itemCount: 6,
-              itemBuilder: (context, index) {
-                return GestureDetector(
-                  onTap: () {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(
-                        content: Text('Image ${index + 1} Clicked!'),
-                      ),
-                    );
-                  },
-                  child: Card(
-                    elevation: 6,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        Image.network(
-                          'https://hips.hearstapps.com/hmg-prod/images/cute-cat-photos-1593441022.jpg?crop=0.670xw:1.00xh;0.167xw,0&resize=640:*',
-                          height: 84,
-                          fit: BoxFit.cover,
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Center(child: Text('Photo ${index + 1}')),
-                        ),
-                      ],
-                    ),
-                  ),
-                );
-              },
-            ),
-            ListTile(
-              leading: Image.network(
-                'https://hips.hearstapps.com/hmg-prod/images/cute-cat-photos-1593441022.jpg?crop=0.670xw:1.00xh;0.167xw,0&resize=640:*',
-                height: 100,
-                width: 80,
-                fit: BoxFit.cover,
-              ),
-              title: Text('Photo 1'),
-              subtitle: Text('Description for Photo 1'),
-            ),
-            ListTile(
-              leading: Image.network(
-                'https://hips.hearstapps.com/hmg-prod/images/cute-cat-photos-1593441022.jpg?crop=0.670xw:1.00xh;0.167xw,0&resize=640:*',
-                height: 100,
-                width: 80,
-                fit: BoxFit.cover,
-              ),
-              title: Text('Photo 2'),
-              subtitle: Text('Description for Photo 2'),
-            ),
-            ListTile(
-              leading: Image.network(
-                'https://hips.hearstapps.com/hmg-prod/images/cute-cat-photos-1593441022.jpg?crop=0.670xw:1.00xh;0.167xw,0&resize=640:*',
-                height: 80,
-                width: 80,
-                fit: BoxFit.cover,
-              ),
-              title: Text('Photo 3'),
-              subtitle: Text('Description for Photo 3'),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: FloatingActionButton(
-                elevation: 18,
-                child: Icon(Icons.upload),
-                onPressed: () {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                      content: Text('Photos Uploaded Successfully!'),
-                    ),
-                  );
-                },
-                //child: Icon(Icons.upload),
-              ),
-            ),
-          ],
-        ),
+      body: ListView.builder(
+        itemCount: shoppingItems.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            leading: Icon(shoppingItems[index].icon),
+            title: Text(shoppingItems[index].name),
+            // Add any additional styling here
+          );
+        },
       ),
     );
   }
+}
+
+class ShoppingItem {
+  final IconData icon;
+  final String name;
+
+  ShoppingItem({required this.icon, required this.name});
 }
